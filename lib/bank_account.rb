0aa0c21bb966fc2ec5f9614d59eacc3e0bcca27e
Chase <chaseit12@gmail.com>
2018-03-12 01:13:56 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,9 @@
 class BankAccount
-  attr_accessor :name, :balance
+  attr_accessor :name, :balance, :status
   
   def initialize(name)
     @name = name
     @balance = balance
+    @status = 'open'
   end
 end
